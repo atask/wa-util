e8d520e4bf6a9acc430ebe22c0a64caccafc1627
ᴉuᴉɥɔsɐʇ uɐllɐ <allan.taschini@gmail.com>
2016-06-16 17:38:51 +0200

    Rename 'user' in 'contact'

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const account = require('./lib/account')
 const chat = require('./lib/chat')
 const message = require('./lib/message')
-const user = require('./lib/user')
+const contact = require('./lib/contact')
 
 module.exports = {
   account,
   chat,
   message,
-  user
+  contact
 }
