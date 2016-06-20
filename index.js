00313a2454a57d5ac857541785c09dd2d284321d
ᴉuᴉɥɔsɐʇ uɐllɐ <allan.taschini@gmail.com>
2016-06-20 15:56:27 +0200

    Fix chat dependency

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 const account = require('./lib/account')
-const chat = require('./lib/chat')
 const message = require('./lib/message')
 const contact = require('./lib/contact')
 
 module.exports = {
   account,
-  chat,
   message,
   contact
 }
