072c63b8f31691532cae8f8a170cb0e526901a05
ᴉuᴉɥɔsɐʇ uɐllɐ <allan.taschini@gmail.com>
2016-06-21 18:03:09 +0200

    Initial CLI interface

--- cli.js
+++ cli.js
@@ -1,36 +1,37 @@
-const account = require('./lib/account')
+const snapshot = require('./lib/snapshot')
 const argv = require('minimist')(process.argv.slice(2))
 const moment = require('moment')
 
-const START_DATE_OPT = 'start-date'
-const END_DATE_OPT = 'end-date'
+const TARGET_DATE_OPT = 'day'
 
-let dbLocation = '.'
+const WA_DB_OPT = 'wa'
+const MSGSTORE_DB_OPT = 'msgstore'
+
 let options = {}
 
-// parse options, if available
-if (argv.f) {
-  dbLocation = argv.f
-} else if (argv.file) {
-  dbLocation = argv.file
+if (argv.d) {
+  options.day = moment(argv.s, 'YYYY-MM-DD')
+} else if (argv[TARGET_DATE_OPT]) {
+  options.day = moment(argv[START_DATE_OPT], 'YYYY-MM-DD')
 }
 
-if (argv.s) {
-  options.startDate = moment(argv.s).format('x')
-} else if (argv[START_DATE_OPT]) {
-  options.startDate = moment(argv[START_DATE_OPT]).format('x')
+if (argv.w) {
+  options.wa = argv.w
+} else if (argv[WA_DB_OPT]) {
+  options.wa = argv[WA_DB_OPT]
 }
 
-if (argv.e) {
-  options.endDate = moment(argv.e).format('x')
-} else if (argv[END_DATE_OPT]) {
-  options.endDate = moment(argv[END_DATE_OPT]).format('x')
+if (argv.m) {
+  options.msgstore = argv.m
+} else if (argv[MSGSTORE_DB_OPT]) {
+  options.msgstore = argv[MSGSTORE_DB_OPT]
 }
 
-let testAccount = Object.create(account)
-testAccount.load(dbLocation, err => {
+snapshot.createSnapshot(options, (err, res) => {
   if (err) {
     console.log('ERROR...')
+    porcess.exit(1)
   }
-  console.log(testAccount.getMediaFilesList())
-}, options)
+  console.log(JSON.stringify(res))
+  process.exit()
+})
